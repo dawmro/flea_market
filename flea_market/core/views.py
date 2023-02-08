c217277988d8dd8266505a08278ed098fa782d66
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-08 10:21:40 +0100

    add contact page

--- flea_market/core/views.py
+++ flea_market/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 # index view
 def index(request):
     return render(request, 'core/index.html')
+
+
+# contact view
+def contact(request):
+    return render(request, 'core/contact.html')
