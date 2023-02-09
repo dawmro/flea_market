00597aed1036e0c618c21c6c0f04cdef7eb8784b
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-09 15:02:04 +0100

    add Item model

--- flea_market/item/models.py
+++ flea_market/item/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-# category database class
+# category model class
 class Category(models.Model):
     name=models.CharField(max_length=255)
 
@@ -15,3 +16,15 @@ class Category(models.Model):
     # display name of category objects
     def __str__(self):
         return self.name
+
+
+# item model class
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    crerated_at = models.DateTimeField(auto_now_add=True)
