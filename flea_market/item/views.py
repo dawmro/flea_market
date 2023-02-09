2088e1e164bd7c10709425405cc860f5943d93e6
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-09 19:48:03 +0100

    display related items

--- flea_market/item/views.py
+++ flea_market/item/views.py
@@ -8,9 +8,12 @@ from .models import Item
 def detail(request, pk):
     # get object from database, Item model, where primary key is pk
     item = get_object_or_404(Item, pk=pk)
+    # get another 3 items from the same category
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[:3]
 
     context = {
-        'item': item
+        'item': item,
+        'related_items': related_items
     }
 
     return render(request, 'item/detail.html', context)
