ae462fe433365e3276bdec2d8424f6ad8311443e
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-09 14:25:54 +0100

    add plural for Category name

--- flea_market/item/models.py
+++ flea_market/item/models.py
@@ -5,3 +5,6 @@ from django.db import models
 # category database class
 class Category(models.Model):
     name=models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
