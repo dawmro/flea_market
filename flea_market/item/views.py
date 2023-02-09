f3cd514e9205256a744c01ce6999dfef8e249b58
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-09 17:53:39 +0100

    add item page

--- flea_market/item/views.py
+++ flea_market/item/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Item
 
 # Create your views here.
+
+# detail view
+def detail(request, pk):
+    # get object from database, Item model, where primary key is pk
+    item = get_object_or_404(Item, pk=pk)
+
+    context = {
+        'item': item
+    }
+
+    return render(request, 'item/detail.html', context)
+
