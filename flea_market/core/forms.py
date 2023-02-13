c6ef71854eaf54955635eebc8e97ed6ff45a70f3
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-13 17:52:25 +0100

    modify looks of form

--- flea_market/core/forms.py
+++ flea_market/core/forms.py
@@ -9,3 +9,24 @@ class SignupForm(UserCreationForm):
         model = User
         # fields for user to fill out
         fields = ('username', 'email', 'password1', 'password2')
+    
+    # modify looks of user field
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your Username goes here',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    # modify looks of email field
+    email = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your Email goes here',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    # modify looks of password1 field
+    password1 = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your Password goes here',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    # modify looks of password2 field
+    password2 = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Repeat Your Password here',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
