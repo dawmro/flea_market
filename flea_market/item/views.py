37a7585d30804f2fc39eb9d03a6d7d8226e6f4c6
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-19 15:47:15 +0100

    add handle POST request

--- flea_market/item/views.py
+++ flea_market/item/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Item
@@ -25,13 +25,30 @@ def detail(request, pk):
 # new item view
 @login_required
 def new_item(request):
-    form = NewItemForm()
+    # handle POST request
+    if request.method == 'POST':
+        # get form data and files uploaded by user
+        form = NewItemForm(request.POST, request.FILES)
+        # if there were no errors
+        if form.is_valid():
+            # create object but do not save it to db yet, because created_by field will not be added, and will result in error
+            item = form.save(commit=False)
+            # set created_by as currently logged in user
+            item.created_by = request.user
+            # now save it
+            form.save()
+            # return view and private key as id of item that just have been created
+            return redirect('item:detail', pk=item.id)
+            
+    # handle GET request        
+    else:
+        form = NewItemForm()
 
-    context = {
-        'form': form,
-        'title': 'New Item'
-    }
-    return render(request, 'item/form.html', context)
+        context = {
+            'form': form,
+            'title': 'New Item'
+        }
+        return render(request, 'item/form.html', context)
 
 
 
