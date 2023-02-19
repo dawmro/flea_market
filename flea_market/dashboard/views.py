9f64a42426661a99038623859d8ea1a92185dee2
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-19 17:45:03 +0100

    add dashboard page

--- flea_market/dashboard/views.py
+++ flea_market/dashboard/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from item.models import Item
 
 # Create your views here.
+
+@login_required
+def index(request):
+    # get all items created by current user
+    items = Item.objects.filter(created_by=request.user)
+
+    context = {
+        'items': items,
+    }
+    return render(request, 'dashboard/index.html', context)
--- flea_market/flea_market/urls.py
+++ flea_market/flea_market/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard', include('dashboard.urls')),
 ]
 
 if settings.DEBUG:
