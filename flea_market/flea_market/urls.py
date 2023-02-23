e178b3c1079889de2af8b58716a96f822baf860f
dawid mrotek <DawidMrotek@protonmail.ch>
2023-02-23 01:59:06 +0100

    add conversation to urlpatterns

--- flea_market/flea_market/urls.py
+++ flea_market/flea_market/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('dashboard', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
 ]
 
 if settings.DEBUG:
